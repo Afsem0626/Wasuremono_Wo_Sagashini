b255e2c857b89d79e48d61acba6c63b1fc793120
Afsem0626 <91596133+Afsem0626@users.noreply.github.com>
2025-06-26 15:32:54 +0900

    プレイヤーと入力状態を管理する構造体を追加した

--- structs.h
+++ structs.h
@@ -1,12 +1,24 @@
-// 構造体ヘッダ
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
 #include <stdbool.h>
+
+// 入力状態を保持する構造体
+typedef struct
+{
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+} InputState;
+
+// プレイヤーの状態を保持する構造体
+typedef struct
+{
+    SDL_Rect rect;
+    // 今後、ここにテクスチャやHPなどを追加していく
+} Player;
 
 // ゲーム全体の状態を管理する構造体
 typedef struct
@@ -15,6 +27,8 @@ typedef struct
     SDL_Renderer *renderer;
     SDL_Joystick *ddrMat;
     bool isRunning;
+    InputState input; // 入力状態を追加
+    Player player;    // プレイヤーを追加
 } GameState;
 
 #endif // STRUCTS_H
