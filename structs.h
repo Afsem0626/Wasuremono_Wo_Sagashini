b99435a430efd52d9ffcc168293d44ba52bcb1cb
Afsem0626 <91596133+Afsem0626@users.noreply.github.com>
2025-06-28 23:11:25 +0900

    input.hに構造体GameObjectを追加，野菜の数，プレイヤのhpなども追加した

--- structs.h
+++ structs.h
@@ -2,7 +2,19 @@
 #define STRUCTS_H
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 #include <stdbool.h>
+
+#define MAX_VEGGIES 3 // 野菜の数
+
+typedef struct
+{
+    bool isActive;
+    SDL_Rect rect;
+    SDL_Texture *texture;
+} GameObject;
 
 // 入力状態を保持する構造体
 typedef struct
@@ -18,6 +30,8 @@ typedef struct
 {
     SDL_Rect rect;
     // 今後、ここにテクスチャやHPなどを追加していく
+    SDL_Texture *texture;
+    int hp;
 } Player;
 
 // ゲーム全体の状態を管理する構造体
@@ -29,6 +43,10 @@ typedef struct
     bool isRunning;
     InputState input; // 入力状態を追加
     Player player;    // プレイヤーを追加
+    GameObject veggies[MAX_VEGGIES];
+    int veggiesCollected;
+    // アセット
+    TTF_Font *font;
 } GameState;
 
 #endif // STRUCTS_H
