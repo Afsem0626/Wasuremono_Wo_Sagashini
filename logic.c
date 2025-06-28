68d7603247312618df204fdec3603499174b600b
Afsem0626 <91596133+Afsem0626@users.noreply.github.com>
2025-06-28 23:20:38 +0900

    logic.cに当たり判定を追加した

--- logic.c
+++ logic.c
@@ -2,6 +2,12 @@
 
 const int PLAYER_SPEED = 8;
 
+// 当たり判定の補助関数
+bool DetectCollision(const SDL_Rect *a, const SDL_Rect *b)
+{
+    return SDL_HasIntersection(a, b);
+}
+
 void UpdateGame(GameState *gs)
 {
     // 入力状態に応じてプレイヤーの座標を更新する
@@ -21,6 +27,16 @@ void UpdateGame(GameState *gs)
     {
         gs->player.rect.x += PLAYER_SPEED;
     }
-
+    // 当たり判定
+    for (int i = 0; i < MAX_VEGGIES; i++)
+    {
+        // アクティブな野菜との当たり判定のみ行う
+        if (gs->veggies[i].isActive && DetectCollision(&gs->player.rect, &gs->veggies[i].rect))
+        {
+            gs->veggies[i].isActive = false; // 野菜を非アクティブに
+            gs->veggiesCollected++;          // カウンタを増やす
+            // ここで効果音を鳴らす処理を入れる (PlaySound()など)
+        }
+    }
     // 画面外に出ないようにする処理 (あとで追加)
 }
