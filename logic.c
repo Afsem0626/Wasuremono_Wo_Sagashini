185f332ce05626bfcb7af8e36cb3c49d172ef341
Afsem0626 <91596133+Afsem0626@users.noreply.github.com>
2025-07-24 14:04:31 +0900

    Expertの時にveggiesRequiredを設定していなかった

--- logic.c
+++ logic.c
@@ -526,6 +526,8 @@ static void ResetStage(GameState *gs)
     case DIFF_IKUU:
         gs->player.hp = 3;
         gs->targetVeggieCount = 5;
+        gs->veggiesRequired = 3;
+
         gs->minigamesRequired = 5;
         gs->stageTimer = 15.0f;
         for (int i = 0; i < MAX_ENEMIES; i++)
